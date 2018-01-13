8a5f492471e5c3b215ee13a15bf82366beb09017
juliazartn <julia.zartn@gmail.com>
2018-01-12 16:15:49 -0800

    finished initial assignment with passing tests

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,53 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        if args.count == 1 {
+            return 0
+        }
+        else if args.count == 3 {
+            let op = args[1]
+            let firstNum = Int(args[0])
+            let secondNum = Int(args[2])
+            switch op {
+            case "-":
+                return firstNum! - secondNum!
+            case "+":
+                return firstNum! + secondNum!
+            case "/":
+                return firstNum! / secondNum!
+            case "%":
+                return firstNum! % secondNum!
+            case "*":
+                return firstNum! * secondNum!
+            default:
+                print("not an operation")
+            }
+        } else {
+            let op = args[args.count-1]
+            switch op {
+            case "count":
+                return args.count - 1
+            case "avg":
+                var index = 1
+                var total = Int(args[0])!
+                while index < args.count - 1 {
+                    total = total + Int(args[index])!
+                    index += 1
+                }
+                return total / (args.count - 1)
+            case "fact":
+                var num = Int(args[0])! - 1
+                var ans = Int(args[0])!
+                while num > 0 {
+                    ans = ans * num
+                    num = num - 1
+                }
+                return ans
+            default:
+                print("not an operation")
+            }
+        }
+        return 0;
     }
     
     public func calculate(_ arg: String) -> Int {
